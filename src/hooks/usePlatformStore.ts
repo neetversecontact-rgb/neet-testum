73b8140bdc2d1ffd0b4fd8b85517a266ff44bb11
neetversecontact-rgb <neetversecontact@gmail.com>
2026-07-01 16:27:46 +0530

    Update usePlatformStore.ts

--- src/hooks/usePlatformStore.ts
+++ src/hooks/usePlatformStore.ts
@@ -1,10 +1,27 @@
 import { useEffect, useState } from "react";
-import { readStore, subscribeStore } from "@/lib/platformStore";
+import { readStore, subscribeStore, Store } from "@/lib/platformStore";
 
 export function usePlatformStore() {
-  const [store, setStore] = useState(() => readStore());
+  const [store, setStore] = useState<Store | null>(null);
 
-  useEffect(() => subscribeStore(() => setStore(readStore())), []);
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchAndSetStore = async () => {
+      const fetchedStore = await readStore();
+      if (isMounted) {
+        setStore(fetchedStore);
+      }
+    };
+
+    fetchAndSetStore();
+    const unsubscribe = subscribeStore(fetchAndSetStore);
+
+    return () => {
+      isMounted = false;
+      unsubscribe();
+    };
+  }, []);
 
   return store;
 }
